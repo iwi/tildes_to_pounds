51d1ef8c063cd3f0b22f6d082fdf5785f38787bb
iwi <isiches@yahoo.com>
2015-10-09 22:27:42 +0100

    Add comments

--- pnc.rb
+++ pnc.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
+#
+# This script substitutes the nth tilde ("~") in each line starting with a "3"
+# of the input file with a pound symbol ("£").
+#
+# Fix from Iu Garcia-Siches 2015-10-09
 
+# Capturing the inputs
 puts "Enter the filename"
 filename = gets.chomp
 puts "Enter the Nth tilde:"
 nth_tilde = gets.chomp.to_i - 1
-# puts "Do you want to create a new file? (Y/N) (Otherwise just lists occurences)"
-# new_file = gets.chomp.upcase
-new_file = "Y"
+puts "Do you want to create a new file? (Y/N) (Otherwise just lists occurences)"
+new_file = gets.chomp.upcase
 
+# Open the output file if needed
 fileOut = File.open("edited_" + filename, 'w') if new_file == 'Y'
 
+# Iterate through the lines of the file and through the tildes of each line
 File.foreach(filename) do |line|
   if line[0] == "3"
     line.gsub!(/~/).each_with_index do |tildes, index|
